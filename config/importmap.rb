7d15dcb6076562dda46372e85a776eb00e1fb8a0
Hamidou Djibo Salha <hamidoudjibosalha@gmail.com>
2025-06-17 10:11:00 +0100

    Pays et Ville

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Il faut toujours inserer cette ligne pour que la suppression delete marche
+pin "@rails/ujs", to: "rails-ujs.js"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_16_085356) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_17_080657) do
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.integer "country_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["country_id"], name: "index_cities_on_country_id"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_16_085356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cities", "countries"
 end
